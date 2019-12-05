525ee77813a444d78d18beb08adabdb9f1915e51
yhe <yhe@ciklum.com>
2019-12-05 17:21:14 +0200

    add platform specific code

--- platform_specific_code/android/app/src/main/kotlin/com/example/platform_specific_code/MainActivity.kt
+++ platform_specific_code/android/app/src/main/kotlin/com/example/platform_specific_code/MainActivity.kt
@@ -9,17 +9,27 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.os.BatteryManager
 import android.os.Build.VERSION
 import android.os.Build.VERSION_CODES
+import android.view.View
+import io.flutter.plugin.common.MessageCodec
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
 
 class MainActivity: FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
 
-    MethodChannel(flutterView(), 'ciklum.flutter.dev/battery').setMethodCallHandler { call, result ->
-      if (call.method == 'getBatteryLevel') {
+    registrarFor("Runner")
+            .platformViewRegistry()
+            .registerViewFactory("MyView", ViewFactory())
+
+    MethodChannel(flutterView, "ciklum.flutter.dev/battery").setMethodCallHandler { call, result ->
+      if (call.method == "getBatteryLevel") {
         val batteryLevel = getBatteryLevel()
 
         if (batteryLevel != -1) {
@@ -32,17 +42,38 @@ class MainActivity: FlutterActivity() {
       }
     }
   }
+
+  private fun getBatteryLevel(): Int {
+    val batteryLevel: Int
+    if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+      val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+      batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+    } else {
+      val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+      batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+    }
+
+    return batteryLevel
+  }
 }
 
-private fun getBatteryLevel(): Int {
-  val batteryLevel: Int
-  if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-    val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
-    batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
-  } else {
-    val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
-    batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+public class ViewFactory(): PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+
+  override fun create(p0: Context?, p1: Int, p2: Any?): PlatformView {
+    return MyView(p0)
+  }
+}
+
+class MyView(val context: Context?): PlatformView {
+
+  override fun dispose() {
+
   }
 
-  return batteryLevel
+  override fun getView(): View {
+    val view = View(context)
+    view.setBackgroundColor(Color.BLUE)
+    return view
+  }
 }
+
--- platform_specific_code/lib/src/test_view_page.dart
+++ platform_specific_code/lib/src/test_view_page.dart
@@ -1,21 +1,32 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class TestViewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Fluff'),
+      appBar: AppBar(
+        title: const Text('Fluff'),
+      ),
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          height: 300,
+          child: platformView(),
         ),
-        body: Center(
-          child: SizedBox(
-            width: 300,
-            height: 300,
-            child: UiKitView(
-              viewType: 'TestView',
-            ),
-          ),
-        ),
+      ),
+    );
+  }
+
+  Widget platformView() {
+    if (Platform.isIOS) {
+      return UiKitView(
+        viewType: 'TestView',
       );
+    }
+    if (Platform.isAndroid) {
+      return AndroidView(viewType: 'MyView',);
+    }
+    return null;
   }
 }
